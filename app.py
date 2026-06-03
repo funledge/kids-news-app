d628f335f0302bd3727ae243bae09e8aa0826734
funledge <funledge9@gmail.com>
2026-06-03 17:19:09 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,22 @@
-import gspread
 from flask import Flask, jsonify
-from google.oauth2.service_account import Credentials
 
 app = Flask(__name__)
 
-SCOPES = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive"
-]
-
-creds = Credentials.from_service_account_file(
-    "kids-news-app-498305-fcc24b7b3575.json",
-    scopes=SCOPES
-)
-
-gc = gspread.authorize(creds)
-
-SPREADSHEET_ID = "1GEjFU21y3oTDaT9QLy_B0am_CWj3UhGwJ0RRPLa1Nyk"
-
-sheet = gc.open_by_key(SPREADSHEET_ID).sheet1
-
+@app.route("/")
+def home():
+    return "Kids News API"
 
 @app.route("/news")
 def news():
-
-    rows = sheet.get_all_records()
-
-    return jsonify(rows)
-
+    return jsonify([
+        {
+            "title": "テストニュース",
+            "summary": "これはテストです",
+            "quiz": "AIとは何でしょう？",
+            "answer": "人工知能",
+            "category": "AI"
+        }
+    ])
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
